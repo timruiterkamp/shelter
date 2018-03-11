b8c92d80422541b9644f32fafa49e484c0857f47
timruiterkamp <timruiterkamp@gmail.com>
2018-03-11 20:18:39 +0100

    added form + possibilty to add to database (nf)

--- server/index.js
+++ server/index.js
@@ -3,19 +3,22 @@
 var express = require('express')
 var db = require('../db')
 var helpers = require('./helpers')
-var find = require('array-find');
+var bodyParser = require('body-parser')
+var slug = require('slug')
 
 module.exports = express()
   .set('view engine', 'ejs')
   .set('views', 'view')
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
+  .use(bodyParser.json())
   .use(express.static('static'))
-  // TODO: Serve the images in `db/image` on `/image`.
   .use('/image', express.static('db/image'))
   .get('/', all)
+  .get('/form', animalForm)
   .get('/:id', animals)
-  /* TODO: Other HTTP methods. */
-  // .post('/', add)
-  // .get('/:id', get)
+  .post('/', addAnimal)
   // .put('/:id', set)
   // .patch('/:id', change)
   .delete('/:id', removeAnimal)
@@ -36,12 +39,12 @@ function animals(req, res) {
   var animalId
 
   try {
-    var animalId = db.has(id)
+    animalId = db.has(id)
   } catch (err) {
     notFound(400, res)
   }
 
-  if(animalId) {
+  if (animalId) {
     var getId = {data: db.get(id)}
 
     res.format({
@@ -51,23 +54,57 @@ function animals(req, res) {
   } else {
     notFound(404, res)
   }
+}
 
+function animalForm(req, res) {
+  res.render('form.ejs')
+}
+
+function addAnimal(req, res) {
+ console.log(req.body)
+ var id = slug(req.body.name).toLowerCase()
+console.log(id)
+
+ var newAnimal = {
+   id: id,
+   name: req.body.name,
+   type: req.body.type,
+   place: req.body.place,
+   sex: req.body.sex,
+   age: req.body.age,
+   vaccinated: req.body.vaccinated,
+   declawed: req.body.declawed,
+   primaryColor: req.body.primaryColor,
+   secondaryColor: req.body.secondaryColor,
+   coat: req.body.coat,
+   weight: req.body.weight,
+   size: req.body.size,
+   length: req.body.length,
+   intake: req.body.intake,
+   description: req.body.description,
+   image: req.body.image
+  }
+
+  try {
+    db.add(newAnimal)
+    res.redirect('/' + id)
+  } catch(err) {
+    notFound(422, res)
+  }
 }
 
 function removeAnimal(req, res) {
- var id = req.params.id
- try {
- db.remove(id)
- res.status(204).end()
- }
- catch(err) {
-   if(db.removed(id)) {
-     notFound(410, res)
-   } else {
-    notFound(404, res)
-   }
-
-}
+  var id = req.params.id
+  try {
+    db.remove(id)
+    res.status(204).end()
+  } catch (err) {
+    if (db.removed(id)) {
+      notFound(410, res)
+    } else {
+      notFound(404, res)
+    }
+  }
 }
 
 function notFound(error, res) {
@@ -77,5 +114,8 @@ function notFound(error, res) {
       title: error
     }]
   }
-  res.status(error).render('error.ejs', errorObj)
+  res.format({
+    json: () => res.status(error).json(errorObj.errors),
+    html: () => res.render('error.ejs', Object.assign({}, errorObj, helpers))
+  })
 }
