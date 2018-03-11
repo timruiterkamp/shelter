408757aa1fd21815be31cd99daf45e179c4884d9
timruiterkamp <timruiterkamp@gmail.com>
2018-03-11 17:07:00 +0100

    Build delete function with 204, 410 and 404 responses

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 var express = require('express')
 var db = require('../db')
 var helpers = require('./helpers')
-var path = require('path');
+var find = require('array-find');
 
 module.exports = express()
   .set('view engine', 'ejs')
@@ -12,24 +12,70 @@ module.exports = express()
   // TODO: Serve the images in `db/image` on `/image`.
   .use('/image', express.static('db/image'))
   .get('/', all)
-
+  .get('/:id', animals)
   /* TODO: Other HTTP methods. */
   // .post('/', add)
   // .get('/:id', get)
   // .put('/:id', set)
   // .patch('/:id', change)
-  // .delete('/:id', remove)
+  .delete('/:id', removeAnimal)
   .listen(1902)
 
 function all(req, res) {
   var result = {errors: [], data: db.all()}
 
-  /* Use the following to support just HTML:  */
-  res.render('list.ejs', Object.assign({}, result, helpers))
-
   /* Support both a request for JSON and a request for HTML  */
-  // res.format({
-  //   json: () => res.json(result),
-  //   html: () => res.render('list.ejs', Object.assign({}, result, helpers))
-  // })
+  res.format({
+    json: () => res.json(result),
+    html: () => res.render('list.ejs', Object.assign({}, result, helpers))
+  })
+}
+
+function animals(req, res) {
+  var id = req.params.id
+  var animalId
+
+  try {
+    var animalId = db.has(id)
+  } catch (err) {
+    notFound(400, res)
+  }
+
+  if(animalId) {
+    var getId = {data: db.get(id)}
+
+    res.format({
+      json: () => res.json(getId),
+      html: () => res.render('detail.ejs', Object.assign({}, getId, helpers))
+    })
+  } else {
+    notFound(404, res)
+  }
+
+}
+
+function removeAnimal(req, res) {
+ var id = req.params.id
+ try {
+ db.remove(id)
+ res.status(204).end()
+ }
+ catch(err) {
+   if(db.removed(id)) {
+     notFound(410, res)
+   } else {
+    notFound(404, res)
+   }
+
+}
+}
+
+function notFound(error, res) {
+  var errorObj = {
+    errors: [{
+      id: error,
+      title: error
+    }]
+  }
+  res.status(error).render('error.ejs', errorObj)
 }
